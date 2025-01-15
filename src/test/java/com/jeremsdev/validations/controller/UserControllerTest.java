8622607374fba28c358c9a5a5584bf5ff4b18085
Jerems412Dev <jeremyobiangdev@gmail.com>
2025-01-15 18:57:47 +0000

    feat: finalized UserControllerTest.

--- src/test/java/com/jeremsdev/validations/controller/UserControllerTest.java
+++ src/test/java/com/jeremsdev/validations/controller/UserControllerTest.java
@@ -7,13 +7,13 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.jeremsdev.validations.dto.LoanDTO;
@@ -41,69 +41,71 @@ public class UserControllerTest {
     private UserService userService;
     @Autowired
     private ObjectMapper objectMapper;
-    private UserDTO user;
+    private UserDTO user1;
     private UserDTO user2;
     private LoanDTO loan1;
     private LoanDTO loan2;
 
     @BeforeEach
-    void init() {
-        user = new UserDTO();
-        user.setName("user1");
-        user.setPhoneNumber("1234567890");
-        user.setEmail("johndoe@example.com");
+    void init() throws ParseException {
+        user1 = new UserDTO();
+        user1.setName("user1");
+        user1.setEmail("user1@example.com");
+        user1.setPhoneNumber("780000000");
 
         user2 = new UserDTO();
-        user2.setName("Obiang Dev");
-        user2.setPhoneNumber("+221 770000000");
-        user2.setEmail("user2@gmail.com");
+        user2.setIdUser(2L);
+        user2.setName("user updated");
+        user2.setEmail("user2@example.com");
+        user2.setPhoneNumber("770000000");
 
         loan1 = new LoanDTO();
-        loan1.setLoanDate(new Date()); // actual Date
-        loan1.setReturnDate(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)); // Return in 7 days
+        loan1.setLoanDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-01-01")); // actual Date
+        loan1.setReturnDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-01-08")); // Return in 7 days
         loan1.setState(true); // Example : active loan
         loan1.setIdUser(1L);
         loan1.setIdBook(1L);
 
         loan2 = new LoanDTO();
-        loan2.setLoanDate(new Date(System.currentTimeMillis() - 14 * 24 * 60 * 60 * 1000)); // loan add 14 days ago
-        loan2.setReturnDate(new Date(System.currentTimeMillis() - 7 * 24 * 60 * 60 * 1000)); // return 7 days ago
+        loan2.setLoanDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-01-01")); // actual Date
+        loan2.setReturnDate(new SimpleDateFormat("yyyy-MM-dd").parse("2023-01-08")); // return 7 days ago
         loan2.setState(false); // Example : loan ended
         loan2.setIdUser(1L);
         loan2.setIdBook(1L);
     }
 
     @Test
-    void shouldUpdateUser() throws Exception {
+    void shouldUpdateUserTwo() throws Exception {
 
-        when(userService.update(anyLong(), any(UserDTO.class))).thenReturn(user);
+        when(userService.update(anyLong(), any(UserDTO.class))).thenReturn(user2);
 
-        this.mockMvc.perform(put("/user/update/{idUser}", 1L)
+        this.mockMvc.perform(put("/user/update/{idUser}", 2L)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
+                        .content(objectMapper.writeValueAsString(user2)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(user.getName())))
-                .andExpect(jsonPath("$.email", is(user.getEmail())))
-                .andExpect(jsonPath("$.phoneNumber", is(user.getPhoneNumber())));
+                .andExpect(jsonPath("$.name", is(user2.getName())))
+                .andExpect(jsonPath("$.email", is(user2.getEmail())))
+                .andExpect(jsonPath("$.phoneNumber", is(user2.getPhoneNumber())));
     }
 
     @Test
-    void shouldFetchOneUserById() throws Exception {
+    void shouldFetchUserByIdUserOne() throws Exception {
 
-        when(userService.getById(anyLong())).thenReturn(user);
+        when(userService.getById(anyLong())).thenReturn(user1);
 
         this.mockMvc.perform(get("/user/getbyid/{idUser}", 1L))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is(user.getName())))
-                .andExpect(jsonPath("$.email", is(user.getEmail())))
-                .andExpect(jsonPath("$.phoneNumber", is(user.getPhoneNumber())));
+                .andExpect(jsonPath("$.name", is(user1.getName())))
+                .andExpect(jsonPath("$.email", is(user1.getEmail())))
+                .andExpect(jsonPath("$.phoneNumber", is(user1.getPhoneNumber())));
     }
 
     @Test
     void shouldFetchAllUsers() throws Exception {
 
         List<UserDTO> list = new ArrayList<>();
-        list.add(user);
+        list.add(user1);
+        list.add(user2);
 
         when(userService.findAll()).thenReturn(list);
 
@@ -117,7 +119,7 @@ public class UserControllerTest {
 
         doNothing().when(userService).delete(anyLong());
 
-        this.mockMvc.perform(delete("/user/delete/{idUser}", 1L))
+        this.mockMvc.perform(delete("/user/delete/{idUser}", 2L))
                 .andExpect(status().isNoContent());
     }
 
@@ -126,11 +128,10 @@ public class UserControllerTest {
 
         List<LoanDTO> list = new ArrayList<>();
         list.add(loan1);
-        list.add(loan2);
 
         when(userService.getLoans(anyLong())).thenReturn(list);
 
-        this.mockMvc.perform(get("/user/getloans/{idUser}"))
+        this.mockMvc.perform(get("/user/getloans/{idUser}",1L))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", is(list.size())));
     }
